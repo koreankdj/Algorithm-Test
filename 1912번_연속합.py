b4b3b7bbe9c4fb6b148fc8af9d9601976905cf73
Dongjin Kim <51356219+koreankdj@users.noreply.github.com>
2023-02-03 16:39:30 +0900

    Update 1912번_연속합.py

--- 1912번_연속합.py
+++ 1912번_연속합.py
@@ -1,2 +1,8 @@
-item = ['1','2','3','4','5']
-print(max(item))
+k = int(input())
+num = list(map(int, input().split()))
+sum = [num[0]]
+
+for i in range(len(num) - 1):
+    sum.append(max(sum[i] + num[i + 1], num[i + 1]))
+
+print(max(sum))
