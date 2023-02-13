23843a138a624da4e15f77a294f405a87812d2d0
Dongjin Kim <51356219+koreankdj@users.noreply.github.com>
2023-02-13 15:07:01 +0900

    Update 1967번_숨바꼭질.py

--- DFS&BFS/1967번_숨바꼭질.py
+++ DFS&BFS/1967번_숨바꼭질.py
@@ -2,34 +2,20 @@
 from collections import deque
 
 def bfs():
-    q = deque()             
-    q.append(n)             
-    check[n] = True
-    dist[n] = 0
-    
+    q = deque()             # deque는 양쪽에서 입출력 가능
+    q.append(n)             # q = deque([5])
     while  q:
-        x = q.popleft()     
-        
+        x = q.popleft()     # 처음 시작점은 x = 5, q = deque([])
         if x == k:
             print(dist[x])
             break
-        if x * 2 <= MAX and check[x*2] == False:        # 순간이동
-            q.append(x*2) 
-            check[x*2] = True
-            dist[x*2] = dist[x]
-        if x + 1 <= MAX and check[x+1] == False:
-            q.append(x+1)
-            check[x+1] = True
-            dist[x+1] = dist[x] + 1
-        if x - 1 >= 0 and check[x-1] == False:
-            q.append(x-1)
-            check[x-1] = True
-            dist[x-1] = dist[x] + 1
-    return 
+        for nx in (x - 1, x + 1, x * 2):    # nx = 4, 6, 10
+            if 0 <= nx <= MAX and not dist[nx]:
+                dist[nx] = dist[x] + 1
+                q.append(nx)    # q = deque([4, 6, "10"])
 
-MAX = 100,000               # 시간초과 안나게 수 제한
-check = [False] * (MAX + 1)
-dist = [-1] * (MAX + 1)      # 이동하는 거리를 알기 위한 변수
+MAX = 10 ** 5               # 시간초과 안나게 수 제한
+dist = [0] * (MAX + 1)      # 이동하는 거리를 알기 위한 변수
 n, k = map(int, input().split())
 
 bfs()
